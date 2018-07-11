4eeaa315b5ff6dd52a5b1d8c7112b580e1e74f01
Remy Pena <rolandojpena88@gmail.com>
2018-07-11 15:26:53 -0500

    added additional components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuestionsComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
