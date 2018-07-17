b6092194d3dab2f29a255c3045c6123aadc4bb20
Unknown <Jonrivals0931@gmail.com>
2018-07-17 16:17:37 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { ResultsComponent } from './results/results.component';
+
+
+// STEP 3 
+const routes: Routes = [
+  // Routes go here
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "questions/:id",
+    component: QuestionsComponent
+  },
+  {
+    path: "results",
+    component: ResultsComponent
+  },
+  {
+    //Default Path Routing
+    path: "",
+    redirectTo: "/",
+    pathMatch: "full"
+  }
+];
+// STEP 4 
+
+// STEP 5
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/assets/Questions .ts
@@ -0,0 +1,313 @@
+// Question 1
+"questions": [
+    {
+        "Id": 1,
+        "Name": "The average person does what thirteen times a day?",
+        "Options": [
+            {
+                "Id": 1,
+                "QuestionId": 1,
+                "Name": "Sighs",
+                "IsAnswer": false
+            },
+            {
+                "Id": 2,
+                "QuestionId": 1,
+                "Name": "Laughs",
+                "IsAnswer": true
+            },
+            {
+                "Id": 2,
+                "QuestionId": 1,
+                "Name": "Eat",
+                "IsAnswer": false
+            },
+            {
+                "Id": 3,
+                "QuestionId": 1,
+                "Name": "Check Their Phone",
+                "IsAnswer": false
+            }
+        ],
+    },
+    // Question 2
+    {
+        "Id": 2,
+        "Name": "The Average American does what 22 times a day?",
+        "Options": [
+            {
+                "Id": 1,
+                "QuestionId": 2,
+                "Name": "Check Their Phone",
+                "IsAnswer": false
+            },
+            {
+                "Id": 2,
+                "QuestionId": 1010,
+                "Name": "Opens Fridge",
+                "IsAnswer": true
+            },
+            {
+                "Id": 1057,
+                "QuestionId": 1010,
+                "Name": "Texts",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1058,
+                "QuestionId": 1010,
+                "Name": "Watches Commercials",
+                "IsAnswer": false
+            }
+        ],
+    },
+    // Question 3
+     {
+        "Id": 1010,
+        "Name": "On Sunday in Florida it is illegal for a single woman to do what?",
+        "Options": [
+            {
+                "Id": 1055,
+                "QuestionId": 1010,
+                "Name": "Get Jobs",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1056,
+                "QuestionId": 1010,
+                "Name": "Skydive",
+                "IsAnswer": true
+            },
+            {
+                "Id": 1057,
+                "QuestionId": 1010,
+                "Name": "Complain",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1058,
+                "QuestionId": 1010,
+                "Name": "Be an S.J.W",
+                "IsAnswer": false
+            }
+        ],
+    },
+    // Question 4
+    {
+        "Id": 1010,
+        "Name": "Washington police officers get a half hour class in how to what?",
+        "Options": [
+            {
+                "Id": 1055,
+                "QuestionId": 1010,
+                "Name": "Eat Doughnuts",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1056,
+                "QuestionId": 1010,
+                "Name": "Sit Down",
+                "IsAnswer": true
+            },
+            {
+                "Id": 1057,
+                "QuestionId": 1010,
+                "Name": "How to use a Tazer",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1058,
+                "QuestionId": 1010,
+                "Name": "Drink Coffee",
+                "IsAnswer": false
+            }
+        ],
+    },
+    // Question 5
+    {
+        "Id": 1010,
+        "Name": "In California you can't legally buy a mousetrap without having a what?",
+        "Options": [
+            {
+                "Id": 1055,
+                "QuestionId": 1010,
+                "Name": "Proof an actual mouse lives in your house",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1056,
+                "QuestionId": 1010,
+                "Name": "Hunting License",
+                "IsAnswer": true
+            },
+            {
+                "Id": 1057,
+                "QuestionId": 1010,
+                "Name": "Giving the mouse a burial",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1058,
+                "QuestionId": 1010,
+                "Name": "Owning an actual Home",
+                "IsAnswer": false
+            }
+        ],
+    },
+    // Question 6
+    {
+        "Id": 1010,
+        "Name": "In Texas it's illegal to swear in front of a what?",
+        "Options": [
+            {
+                "Id": 1055,
+                "QuestionId": 1010,
+                "Name": "Children",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1056,
+                "QuestionId": 1010,
+                "Name": "Corpse",
+                "IsAnswer": true
+            },
+            {
+                "Id": 1057,
+                "QuestionId": 1010,
+                "Name": "Infants",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1058,
+                "QuestionId": 1010,
+                "Name": "Priests",
+                "IsAnswer": false
+            }
+        ],
+    },
+    // Question 7
+            {
+        "Id": 1010,
+        "Name": "In South Dakota it's illegal to fall down and sleep where?",
+        "Options": [
+            {
+                "Id": 1055,
+                "QuestionId": 1010,
+                "Name": "In the Bushes",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1056,
+                "QuestionId": 1010,
+                "Name": "In a Cheese Factory",
+                "IsAnswer": true
+            },
+            {
+                "Id": 1057,
+                "QuestionId": 1010,
+                "Name": "In a Mattress Furniture Store",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1058,
+                "QuestionId": 1010,
+                "Name": "In a Park",
+                "IsAnswer": false
+            }
+        ],
+    },
+    // Question 8
+    {
+        "Id": 1010,
+        "Name": "It's illegal in Georgia to do what with a fork?",
+        "Options": [
+            {
+                "Id": 1055,
+                "QuestionId": 1010,
+                "Name": "Clean Teeth",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1056,
+                "QuestionId": 1010,
+                "Name": "Eat fried chicken",
+                "IsAnswer": true
+            },
+            {
+                "Id": 1057,
+                "QuestionId": 1010,
+                "Name": "kill a Lizard",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1058,
+                "QuestionId": 1010,
+                "Name": "Eat Pizza",
+                "IsAnswer": false
+            }
+        ],
+    },
+    // Question 9
+    {
+        "Id": 1010,
+        "Name": "What ailment kills the most fruit flies?",
+        "Options": [
+            {
+                "Id": 1055,
+                "QuestionId": 1010,
+                "Name": "Zika",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1056,
+                "QuestionId": 1010,
+                "Name": "Constipation",
+                "IsAnswer": true
+            },
+            {
+                "Id": 1057,
+                "QuestionId": 1010,
+                "Name": "Gingivitis",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1058,
+                "QuestionId": 1010,
+                "Name": "Fungi",
+                "IsAnswer": false
+            }
+        ],
+    },
+    // Question 10
+    {
+        "Id": 1010,
+        "Name": "It's illegal in Texas to put what on your neighbors Cow?",
+        "Options": [
+            {
+                "Id": 1055,
+                "QuestionId": 1010,
+                "Name": "Poncho",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1056,
+                "QuestionId": 1010,
+                "Name": "Graffiti",
+                "IsAnswer": true
+            },
+            {
+                "Id": 1057,
+                "QuestionId": 1010,
+                "Name": "Leather Collar",
+                "IsAnswer": false
+            },
+            {
+                "Id": 1058,
+                "QuestionId": 1010,
+                "Name": "Bronze Bell",
+                "IsAnswer": false
+            }
+        ],
+    },
+    
+]
