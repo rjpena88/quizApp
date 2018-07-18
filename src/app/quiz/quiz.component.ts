adb99d823e1e346c8fd76b716af731ff68b6ca43
Roxanne Garcia <rxrgeneration@gmail.com>
2018-07-18 16:41:00 -0500

    tally attempt

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -11,6 +11,7 @@ import { Option, Question, Quiz, QuizConfig} from '../models/index';
   providers: [QuizService]
 })
 export class QuizComponent implements OnInit {
+  tally: 0;
   quizes: any[];
   quiz: Quiz = new Quiz(null);
   mode = 'home';
@@ -19,7 +20,7 @@ export class QuizComponent implements OnInit {
     'allowBack': true,
     'allowReview': true,
     'autoMove': false,  // if true, it will move to next question automatically when answered.
-    'duration': 120,  // indicates the time (in secs) in which quiz needs to be completed. 0 means unlimited.
+    'duration': 60,  // indicates the time (in secs) in which quiz needs to be completed. 0 means unlimited.
     'pageSize': 1,
     'requiredAll': false,  // indicates if you must answer all the questions before submitting.
     'richText': false,
@@ -30,6 +31,10 @@ export class QuizComponent implements OnInit {
     'theme': 'none'
   };
 
+// This will be gloable variable for TALLY
+
+
+  // Figure this out
   pager = {
     index: 0,
     size: 1,
@@ -83,16 +88,52 @@ export class QuizComponent implements OnInit {
       this.quiz.questions.slice(this.pager.index, this.pager.index + this.pager.size) : [];
   }
 
+  // Checking if it's selected
   onSelect(question: Question, option: Option) {
+    // let tally = question.tally;
     if (question.questionTypeId === 1) {
       question.options.forEach((x) => { if (x.id !== option.id) x.selected = false; });
+      // this.tally++;
+      // console.log(this.tally);
+      
     }
+    // console.log("this is the ALL options")
 
+  //   for (var i = 0; i < a.length; i++) {
+  //     if (a[i] == 1) a.push(5);
+  //     console.log(a[i]);
+  // }
+    
+  //   console.log(question.options);
+    if(question.questionTypeId === 1){
+      question.options.forEach((answer) => { 
+        if(answer.selected == true && answer.isAnswer == true){
+          this.tally++;
+          console.log(this.tally);
+          // console.log("This is the Correct ANSWER!!!")
+          // console.log(answer);
+          // question.tally.push(answer.name);
+          // console.log("I am the tally");
+          // console.log(question.tally);
+
+          
+          // this.tally = answer;
+          // console.log("this is the TALLY");
+          // console.log(this.tally)
+
+      }
+    })
+
+
+  }
+
+    // this changes index to go to next question
     if (this.config.autoMove) {
       this.goTo(this.pager.index + 1);
     }
   }
 
+  // How the index workds
   goTo(index: number) {
     if (index >= 0 && index < this.pager.count) {
       this.pager.index = index;
@@ -105,15 +146,40 @@ export class QuizComponent implements OnInit {
   };
 
   isCorrect(question: Question) {
-    return question.options.every(x => x.selected === x.isAnswer) ? 'correct' : 'wrong';
+    // let result2 = question.tally;
+    // let result = [];
+    // return "hey";
+    let optionTest = question.options;
+  //  return question.options.every(x => x.selected === x.isAnswer) ? 'correct' : 'wrong';
+   let style = question.options.every(x => x.selected === x.isAnswer) ? 'correct' : 'wrong';
+  //  console.log(x);
+   return style;
+
+    // let test = question.options;
+    // console.log(test);
+    // return style;
+
+    
+    
+    // console.log(x);
   };
 
   onSubmit() {
-    let answers = [];
-    this.quiz.questions.forEach(x => answers.push({ 'quizId': this.quiz.id, 'questionId': x.id, 'answered': x.answered }));
+    // let answers = [];
+    // let tally = [];
+    // console.log("this is FIRST QUESTION");
+    // let optionsTarget = this.quiz.questions[0];
+    // console.log("this is the Target Log");
+    // console.log( optionsTarget);
+    // console.log(this.quiz.questions.options[0]);
+    // this.quiz.questions.forEach(x => answers.push({ 'quizId': this.quiz.id, 'questionId': x.id, 'answered': x.answered }));
+    // If this.quiz.questions.
+    // this.quiz.questions.forEach(a => tally.push({ 'options': this.quiz.questions.options,  }));
+    // console.log("This is the new answers array");
+    // console.log(answers);
 
     // Post your data to the server here. answers contains the questionId and the users' answer.
-    console.log(this.quiz.questions);
+    // console.log(this.quiz.questions);
     this.mode = 'result';
   }
 }
