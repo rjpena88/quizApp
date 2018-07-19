5a4a7d1b5d46839aeef306d47a3d49ec567a55b1
Jeremy P <rolandojpena88@gmail.com>
2018-07-19 10:16:01 -0500

    added working meter to results

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -140,79 +140,79 @@ export class QuizComponent implements OnInit {
     this.mode = 'result';
   }
 }
-var chart = AmCharts.makeChart("chartdiv", {
-  "theme": "light",
-  "type": "gauge",
-  "axes": [{
-    // inner circle
-    "id": "axis1",
-    "labelsEnabled": false,
-    "axisColor": "#808080",
-    "axisAlpha": 1,
-    "tickAlpha": 0,
-    "radius": "20%",
-    "startAngle": 0,
-    "endAngle": 360,
-    "topTextFontSize": 20,
-    "topTextYOffset": 140,
-    "topText": "Goal: 4.0"
-  }, {
-    // red ticks
-    "id": "axis2",
-    "endAngle": 70,
-    "endValue": 400,
-    "radius": "100%",
-    "axisAlpha": 0,
-    "axisThickness": 0,
-    "valueInterval": 4,
-    "minorTickInterval": 4,
-    "tickColor": "#ff0000",
-    "tickLength": 40,
-    "labelsEnabled": true,
-    "labelFrequency": 100,
-    "labelFunction": createLabel,
-    // text inside center circle
-    "topTextFontSize": 20,
-    "topTextYOffset": 130,
-    "topTextColor": "#808080",
-  }, {
-    // green ticks
-    "id": "axis2",
-    "startAngle": 70,
-    "startValue": 400,
-    "endValue": 500,
-    "radius": "100%",
-    "valueInterval": 4,
-    "minorTickInterval": 4,
-    "axisAlpha": 0,
-    "axisThickness": 0,
-    "tickColor": "#006600",
-    "tickLength": 40,
-    "labelsEnabled": true,
-    "labelFrequency": 25,
-    "labelFunction": createLabel,
-  }],
-  "arrows": [{
-    "axis": "axis2",
-    "color": "#808080",
-    "innerRadius": "20%",
-    "nailRadius": 10,
-    "radius": "70%"
-  }],
-  "export": {
-    "enabled": true
-  }
-});
+// var chart = AmCharts.makeChart("chartdiv", {
+//   "theme": "light",
+//   "type": "gauge",
+//   "axes": [{
+//     // inner circle
+//     "id": "axis1",
+//     "labelsEnabled": false,
+//     "axisColor": "#808080",
+//     "axisAlpha": 1,
+//     "tickAlpha": 0,
+//     "radius": "20%",
+//     "startAngle": 0,
+//     "endAngle": 360,
+//     "topTextFontSize": 20,
+//     "topTextYOffset": 140,
+//     "topText": "Goal: 4.0"
+//   }, {
+//     // red ticks
+//     "id": "axis2",
+//     "endAngle": 70,
+//     "endValue": 400,
+//     "radius": "100%",
+//     "axisAlpha": 0,
+//     "axisThickness": 0,
+//     "valueInterval": 4,
+//     "minorTickInterval": 4,
+//     "tickColor": "#ff0000",
+//     "tickLength": 40,
+//     "labelsEnabled": true,
+//     "labelFrequency": 100,
+//     "labelFunction": createLabel,
+//     // text inside center circle
+//     "topTextFontSize": 20,
+//     "topTextYOffset": 130,
+//     "topTextColor": "#808080",
+//   }, {
+//     // green ticks
+//     "id": "axis2",
+//     "startAngle": 70,
+//     "startValue": 400,
+//     "endValue": 500,
+//     "radius": "100%",
+//     "valueInterval": 4,
+//     "minorTickInterval": 4,
+//     "axisAlpha": 0,
+//     "axisThickness": 0,
+//     "tickColor": "#006600",
+//     "tickLength": 40,
+//     "labelsEnabled": true,
+//     "labelFrequency": 25,
+//     "labelFunction": createLabel,
+//   }],
+//   "arrows": [{
+//     "axis": "axis2",
+//     "color": "#808080",
+//     "innerRadius": "20%",
+//     "nailRadius": 10,
+//     "radius": "70%"
+//   }],
+//   "export": {
+//     "enabled": true
+//   }
+// });
 
-setInterval(randomValue, 2000);
+// setInterval(randomValue, 2000);
 
-// set random value
-function randomValue() {
-  var value = Math.round(Math.random() * 500);
-  chart.arrows[0].setValue(value);
-  chart.axes[1].setTopText(value / 100);
-}
+// // set random value
+// function randomValue() {
+//   var value = Math.round(Math.random() * 500);
+//   chart.arrows[0].setValue(value);
+//   chart.axes[1].setTopText(value / 100);
+// }
 
-function createLabel(value) {
-  return value / 100.0
-}
+// function createLabel(value) {
+//   return value / 100.0
+// }
